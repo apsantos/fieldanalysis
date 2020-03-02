83bf0da309ce16f612fb04eb4e7ac0f8d1ee0f15
apsantos <asanto@uno-login1.sandia.gov>
2020-03-02 16:28:15 -0700

    Add to the bash script inputs

--- analyze_bash.sh
+++ analyze_bash.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 rho=$1
-dmft_slice.py 25 25 315 250 2 avg_rhodb.dat $rho
+Mx=$2
+My=$3
+Mz=$4
+Lz=$5
+dmft_slice.py $Mx $My $Mz $Lz 2 avg_rhodb.dat $rho
 com2.py dens_no-shift_slice.dat tmp.dat 
-fit_curve.py 1 tmp.dat tmp2.dat 1
+fit_curve.py 1 tmp.dat tmp2.dat 1 > fits 
