5a87071b8e3fb5b9c21a162bcb3030e9e73c91f4
Santos <asanto@s1037600.srn.sandia.gov>
2020-03-02 17:27:12 -0700

    add help sections and improve readability of dmft_slice.py and fit_curve.py

--- dmft_slice.py
+++ dmft_slice.py
@@ -4,37 +4,58 @@ import os, sys
 import cmath as cm
 import math as m
 import fts_utils as fts
+import sys, argparse
 
-if (len(sys.argv) < 7):
-  print "Usage: 3D_dens_slice.py [Nx] [Ny] [Nz] [L-direction] [direction] [density file] [rho0]"
-  exit(1)
+def main(argv=None):
+    # Parse in command-line arguments, and create the user help instructions
+    parser = argparse.ArgumentParser(description='Average meshed-field density'
+                                                 'and convert to 1-d density profile')
+    parser.add_argument("--mesh_file", type=str, required=True,
+                   help='input simulation meshed-field density file')
+    parser.add_argument("--profile_file", type=str, default='dens_no-shift_slice.dat',
+                   help='output averaged density profile file')
+    parser.add_argument("--L_direction", type=float, required=True,
+                   help='length of simulation box in averaging direction')
+    parser.add_argument("--direction", type=str, choices=['x', 'y', 'z'], default='z',
+                   help='averaging direction')
+    parser.add_argument("--mesh", type=int, nargs='+', required=True,
+                   help='mesh size in the x-,y- and z-directions.')
+    parser.add_argument("--density", type=float, required=True,
+                   help='density, rho_0, in field part of simulation')
 
-Nx = nmp.zeros(3,'int')
-Nx[0] = int( sys.argv[1] )
-Nx[1] = int( sys.argv[2] )
-Nx[2] = int( sys.argv[3] )
-L = float(sys.argv[4])
-dirs = nmp.zeros(3, 'int')
-loop_dir = int( sys.argv[5] )
-#notloop = nmp.where(dirs != loop_dir)[0]
-#Mnl = Nx[ notloop[0] ] * Nx[ notloop[1] ]
+    if len(parser.parse_args().mesh) != 3:
+        print "--mesh must have 3 values for x-, y- and z-direction"
+        return
+        
+    Nx = parser.parse_args().mesh
+    L = parser.parse_args().L_direction
+    if parser.parse_args().direction == 'x':
+        loop_dir = 0
+    elif parser.parse_args().direction == 'y':
+        loop_dir = 1
+    elif parser.parse_args().direction == 'z':
+        loop_dir = 2
+    
+    # Read in density file
+    x, y, z, rho = fts.read_real_3d_dat(Nx[0], Nx[1], Nx[2], parser.parse_args().mesh_file)
+    
+    # Calculate density slice
+    slice, std = fts.avg_3d_full_slice_no_shift(Nx[0], Nx[1], Nx[2], loop_dir, rho)
+    
+    # Write output
+    otp = open(parser.parse_args().profile_file,"w")
+    
+    #dx = x[1] - x[0]
+    dx = L / Nx[loop_dir]
+    
+    rho0 = parser.parse_args().density
+    otp.write("# %s position real_rho real_rho_std imaginary_rho imaginary_rho_std\n" % parser.parse_args().direction)
+    for i in range(0, Nx[loop_dir]):
+        line = "%lf %e %e %e %e\n" % (i*dx/L, slice[i].real/rho0, std[i].real/rho0 , 
+            slice[i].imag/rho0 , std[i].imag/rho0 )
+        otp.write(line)
+    
+    otp.close()
 
-# Read in density file
-x, y, z, rho = fts.read_real_3d_dat(Nx[0], Nx[1], Nx[2], sys.argv[6])
-
-# Calculate density slice
-slice , std = fts.avg_3d_full_slice_no_shift(Nx[0], Nx[1], Nx[2], loop_dir, rho)
-
-# Write output
-otp = open("dens_no-shift_slice.dat","w")
-
-#dx = x[1] - x[0]
-dx = L / Nx[loop_dir]
-
-rho0 = float(sys.argv[7])
-for i in range(0, Nx[loop_dir]):
-  line = "%lf %e %e %e %e\n" % (i*dx/L, slice[i].real/rho0, std[i].real/rho0 , 
-      slice[i].imag/rho0 , std[i].imag/rho0 )
-  otp.write(line)
-
-otp.close()
+if __name__ == '__main__':
+    sys.exit(main())
--- fit_curve.py
+++ fit_curve.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import sys, argparse
 import math as m
 import cmath as cm
 import os, sys
@@ -10,72 +11,67 @@ from scipy.fftpack import fft, ifft
 from scipy.optimize import curve_fit
 #import matplotlib.pyplot as plt
 
-def func_fit(x,rho_g, rho_l, xi, frac):
-  #print rho_g, rho_l, xi, frac  
-  return rho_g + 0.5*(rho_l - rho_g) * scipy.special.erf(m.sqrt(m.pi) * (x - Lx/2.0 + frac) / xi) - 0.5*(rho_l - rho_g) * scipy.special.erf(m.sqrt(m.pi) * (x - Lx/2.0 - frac) / xi)
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 
-if ( len( sys.argv ) < 3 ):
-    print "Usage: radial_avg_rho.py [Lx] [input.dat] [output.dat] [iter number]"
+class ERFprofile(object):
+    def erf_profile(self, x, rho_g, rho_l, xi, frac):
+        return rho_g + 0.5*(rho_l - rho_g) * scipy.special.erf(m.sqrt(m.pi) * (x - self.L/2.0 + frac) / xi) - 0.5*(rho_l - rho_g) * scipy.special.erf(m.sqrt(m.pi) * (x - self.L/2.0 - frac) / xi)
+    
+def main(argv=None):
+    # Parse in command-line arguments, and create the user help instructions
+    parser = argparse.ArgumentParser(description='Average meshed-field density'
+                                                 'and convert to 1-d density profile')
+    parser.add_argument("--profile_file", type=str, required=True, help='input density profile file')
+    parser.add_argument("--fit_file", type=str, default='fitted_profile.dat', help='output fitted density profile file')
+    parser.add_argument("--L_direction", type=float, default=1, help='length of simulation box in averaging direction')
 
-else:
-  Lx = float(sys.argv[1])
-  inp = open(sys.argv[2], 'r') 
+    L = parser.parse_args().L_direction
 
-  line = inp.readline().split()
+    ndat = file_len(parser.parse_args().profile_file)
 
-  ndat = 0
+    x = np.zeros(ndat,'d')
+    y = np.zeros(ndat,'d')
 
-  while (len(line) > 0):
-    if float(line[1]) > 0.0:
-      ndat += 1
-    line = inp.readline().split()
+    inp = open(parser.parse_args().profile_file, 'r') 
 
-  #print ndat
+    for i in range(0,ndat):
+      line = inp.readline().split()
+      x[i] = float(line[0])
+      y[i] = float(line[1]) 
 
-  inp.close()
+    guess= np.zeros(4,'d')
+    guess[0] = 0.1
+    guess[1] = 0.9
+    guess[2] = 0.1
+    guess[3] = 0.1
 
-  x = np.zeros(ndat,'d')
-  y = np.zeros(ndat,'d')
-
-  inp = open(sys.argv[2], 'r')
-
-  line = inp.readline().split()
-
-  for i in range(0,ndat):
-    x[i] = float(line[0])
-    y[i] = float(line[1]) 
-    line = inp.readline().split()
-
-  guess= np.zeros(4,'d')
-  guess[0] = 0.1
-  guess[1] = 0.9
-  guess[2] = 0.1
-  guess[3] = 0.1
-  #print guess
-
-  popt, pcov = curve_fit(func_fit,x,y,guess,maxfev = 100000)
+    # set the L in the function
+    profile = ERFprofile()
+    profile.L = L
+    popt, pcov = curve_fit(profile.erf_profile,x,y,guess,maxfev = 100000)
  
-  #print popt;
+    n = 0
+    while(n < 100):
+      popt, pcov = curve_fit(profile.erf_profile,x,y,popt,maxfev = 100000)
+      n += 1
+    print "# rho_g rho_l xi frac uncertainties
+    print popt[0], popt[1], popt[2], popt[3], pcov[0], pcov[1], pcov[2], pcov[3]
 
-  n = 0
-  while(n < 100):
-    popt, pcov = curve_fit(func_fit,x,y,popt,maxfev = 100000)
-    n += 1
-  print popt[0], popt[1], popt[2], popt[3], pcov[0], pcov[1], pcov[2], pcov[3]
+    fit = np.zeros(ndat,'d')
+    for i in range(0,ndat):
+      #fit[i] = popt[0] + 0.5*(popt[1] - popt[0]) * scipy.special.erf(m.sqrt(m.pi) * (x[i] - L/2.0 + popt[3]) / popt[2]) - 0.5*(popt[1] - popt[0]) * scipy.special.erf(m.sqrt(m.pi) * (x[i] - L/2.0 - popt[3]) / popt[2])
+      fit[i] = profile.erf_profile(x[i], popt[0], popt[1], popt[2], popt[3])
 
-  fit = np.zeros(ndat,'d')
-  for i in range(0,ndat):
-    fit[i] = popt[0] + 0.5*(popt[1] - popt[0]) * scipy.special.erf(m.sqrt(m.pi) * (x[i] - Lx/2.0 + popt[3]) / popt[2]) - 0.5*(popt[1] - popt[0]) * scipy.special.erf(m.sqrt(m.pi) * (x[i] - Lx/2.0 - popt[3]) / popt[2])
+    otp = open(parser.parse_args().fit_file, 'w') 
+    for i in range(0,ndat):
+      line = '%lf %lf\n' % (x[i], fit[i])
+      otp.write(line)
 
-  otp = open(sys.argv[3], 'w')
-  for i in range(0,ndat):
-    line = '%lf %lf\n' % (x[i], fit[i])
-    otp.write(line)
+    otp.close()
 
-  iter_num = int(sys.argv[4])
-  #print 100*100*(popt[2] * popt[2] / 2.0 / m.pi)
-  #print popt[0]
-  #print popt[1]
-
-
-  otp.close()
+if __name__ == '__main__':
+    sys.exit(main())
