533df159614bac10d0b8d7367f6f20c23d4c02db
apsantos <a.p.santos11@gmail.com>
2021-06-08 17:25:01 -0600

    Add gibbs dividing surface part to excess_adsorption

--- excess_adsorption.py
+++ excess_adsorption.py
@@ -9,6 +9,11 @@ def file_len(fname):
             pass
     return i + 1
 
+def smooth(y, box_pts):
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
+
 def main(argv=None):
     # Parse in command-line arguments, and create the user help instructions
     parser = argparse.ArgumentParser(description='Fit vapor, liquid density to get critical point and curve')
@@ -19,7 +24,7 @@ def main(argv=None):
     parser.add_argument("--r_max", type=float, default=1000000.0, help='radius min to integrate')
     parser.add_argument("--rho_col", type=int, default=1, help='density data collumn')
     parser.add_argument("--rho_inf", type=float, default=1.0, help='the far-field density')
-
+    parser.add_argument("--gibbssurface", action='store_true', default=False, help='Find the gibbs dividing surface and use as the low r limit')
     c_r = parser.parse_args().r_col
     c_rho = parser.parse_args().rho_col
     rho_inf = parser.parse_args().rho_inf
@@ -32,6 +37,7 @@ def main(argv=None):
 
     j = 0
     r = []
+    rho = []
     deltarho = []
     for line in inp:
         data = line.strip().split()
@@ -41,13 +47,52 @@ def main(argv=None):
         tr = float(data[c_r])
         if r_min < tr < r_max:
             r.append( tr )
+            rho.append( float(data[c_rho]) )
             deltarho.append( float(data[c_rho])-rho_inf )
 
     r = np.array(r)
     deltarho = np.array(deltarho)
+    if parser.parse_args().gibbssurface:
+        otp = open(parser.parse_args().profile_file+'.derivative', 'w') 
+        otp.write("# r rho smooth_rho drhodr integrand\n" )
+        # get derivative of rho
+        smooth_rho = smooth(rho,3)
+        dr = r[1]-r[0]
+        #drhodr = np.gradient(smooth_rho, 4*(r[1]-r[0]))
+        drhodr = np.gradient(rho, dr)
+        cutoff=0
+        integrand = np.zeros((len(drhodr)-cutoff))
+        for i in range(len(integrand)):
+            V_bin = 3.*dr*r[i]**2. + dr**3./4.
+            integrand[i] = rho[i]*V_bin
+            #integrand[i] = drhodr[i]*V_bin
+            #integrand[i] = drhodr[i]*r[i]**3.0
+            otp.write("%f %f %f %f %f\n" % ( r[i], rho[i], smooth_rho[i], drhodr[i], integrand[i] ) )
+
+        # liquid droplet tpye
+        #re = (1/(0.0-rho_inf) * integrate.simps(integrand,x=r) )**(1./3.)
+        # assuming it is away from a surface
+        #re = ((1.0/rho_inf * integrate.simps(integrand,x=r[:len(r)-cutoff])))**(1./3.)
+        #re = ( (1.0/rho_inf * integrate.simps(integrand,x=r[:len(r)-cutoff])))**(1./3.)
+        #N = integrate.simps(integrand,x=r[:len(r)-cutoff])
+        N = np.sum(integrand)
+        Ve = max(0.0,r_max**3.0 - N/rho_inf)
+        re = (Ve)**(1./3.)
+        #print r_max, N
+        for ire in range(len(r)):
+            if r[ire] > re:
+                break
+        deltarho = deltarho[ire:]
+        r = r[ire:]
+        
+        
     integral = integrate.simps(deltarho,x=r)
     
-    print integral
+    if parser.parse_args().gibbssurface:
+        print integral, re
+    else:
+        print integral
+
 if __name__ == '__main__':
     sys.exit(main())
 
