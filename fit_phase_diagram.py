e5bcd41df66335c2ca2374a5c833e96a74672634
apsantos <a.p.santos11@gmail.com>
2021-06-08 17:23:40 -0600

    Add critical point extrapolating script option for lcst and ucst

--- fit_phase_diagram.py
+++ fit_phase_diagram.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
+# !/usr/bin/python
 import sys, argparse
 import math as ma
 import numpy as np
@@ -16,7 +17,9 @@ def file_len(fname):
 class FitPhaseCoex(object):
     def __init__(self):
         self.beta = 0.325
-        self.mu = 1.0 #0.872
+        self.mu =0.872
+        #self.mu = 1.0 #0.872
+        self.sign = 1.0
 
     def objective(self, params, x, data):
         """ calculate total residual for fits to several data sets held
@@ -34,18 +37,20 @@ class FitPhaseCoex(object):
         T_c = B['T_c'].value
         delta_rho_0 = B['delta_rho_0'].value
 
-        return delta_rho_0 * (T_c - T)**self.beta
+        return delta_rho_0 * (self.sign*(T_c-T))**self.beta
+        #return delta_rho_0 * ((T_c - T))**self.beta
 
     def thermo(self, B, T):
         rho_c = B['rho_c'].value
         T_c = B['T_c'].value
         A = B['A'].value
 
-        return rho_c + (A * (T_c - T)**self.mu)
+        return rho_c + (A * (self.sign*(T_c - T))**self.mu)
 
     def density_set(self, T):
-        avg_rho = self.rho_c + self.A * (self.T_c - T)
-        delta_rho = self.delta_rho_0 * (self.T_c - T)**self.beta
+        avg_rho = self.rho_c + self.A * self.sign * (self.T_c - T)
+        delta_rho = self.delta_rho_0 * (self.sign * (self.T_c - T))**self.beta
+        #delta_rho = self.delta_rho_0 * (self.sign * (1-(T/self.T_c)))**self.beta
         rho_l = avg_rho + delta_rho/2.0
         rho_v = avg_rho - delta_rho/2.0
         return rho_v, rho_l
@@ -59,15 +64,26 @@ def main(argv=None):
     parser.add_argument("--temp_col", type=int, default=0, help='temperature col')
     parser.add_argument("--vapor_col", type=int, default=1, help='vapor col')
     parser.add_argument("--liquid_col", type=int, default=2, help='liquid col')
+    parser.add_argument("--type", type=str, choices=['lcst','ucst'],default='ucst', help='Is this an upper or lower critcal solution temperature transition?')
+    parser.add_argument("--Tmin", type=float, help='Default is to ignore and use full T range.')
+    parser.add_argument("--Tmax", type=float, help='Default is to ignore and use full T range.')
 
     c_t = parser.parse_args().temp_col
     c_v = parser.parse_args().vapor_col
     c_l = parser.parse_args().liquid_col
     ndat = file_len(parser.parse_args().profile_file) - parser.parse_args().start_line
+
+    if parser.parse_args().Tmin:
+        Tmin = parser.parse_args().Tmin
+    if parser.parse_args().Tmax:
+        Tmax = parser.parse_args().Tmax
+
     T = np.zeros(ndat,'d')
     rho_v = np.zeros(ndat,'d')
     rho_l = np.zeros(ndat,'d')
-    fit_data = np.zeros((2,ndat),'d')
+    fitT = []
+    fitrho_v = []
+    fitrho_l = []
 
     # read in temeprature and vapor/liquid densities
     inp = open(parser.parse_args().profile_file, 'r') 
@@ -82,23 +98,52 @@ def main(argv=None):
         T[i] = float(data[c_t])
         rho_v[i] = float(data[c_v]) 
         rho_l[i] = float(data[c_l]) 
+        if Tmin < T[i] and T[i] < Tmax:
+            fitT.append( float(data[c_t]) )
+            fitrho_v.append( float(data[c_v]) ) 
+            fitrho_l.append( float(data[c_l]) )
+        i += 1
+    nfitdata = len(fitT)
+    fitT = np.array(fitT)
+    print fitT
+    fit_data = np.zeros((2,nfitdata),'d')
+    for i in range(nfitdata):
         rho_mean = (rho_v[i]+rho_l[i])/2.0
         delta_rho = rho_l[i] - rho_v[i]
         fit_data[:,i] = [rho_mean,delta_rho]
-        i += 1
 
-    T_c_guess = max(T)
+    if parser.parse_args().type == 'ucst':
+        T_c_guess = max(T)*1.01
+        T_c_min = T_c_guess
+        if T_c_guess > 0:
+            T_c_max = 5.0*T_c_guess
+        else:
+            T_c_max = 0.5*T_c_guess
+    elif parser.parse_args().type == 'lcst':
+        T_c_guess = min(T) * 0.99
+        T_c_max = T_c_guess
+        if T_c_guess > 0:
+            T_c_min = 0.5*T_c_guess
+        else:
+            T_c_min = 5.0*T_c_guess
+
     rho_c_guess = (max(rho_v)+min(rho_l))/2.0
 
+    print T_c_min, T_c_max
     fit_params = Parameters()
     fit_params.add( 'rho_c', value=rho_c_guess, min=max(rho_v),  max=min(rho_l))
-    fit_params.add( 'T_c', value=T_c_guess, min=T_c_guess,  max=100*T_c_guess)
-    fit_params.add( 'A', value=0.1, min=0.0,  max=2.0)
+    fit_params.add( 'T_c', value=T_c_guess, min=T_c_min, max=T_c_max)
+    fit_params.add( 'A', value=0.1, min=0.0,  max=100.0)
     fit_params.add( 'delta_rho_0', value=0.1, min=0.0, max=3.0)
     
     profile = FitPhaseCoex()
+    if parser.parse_args().type == 'ucst':
+        profile.sign = 1.0
+    elif parser.parse_args().type == 'lcst':
+        profile.sign = -1.0
     
-    minner = Minimizer(profile.objective, fit_params, fcn_args=(T, fit_data), calc_covar=True)
+    minner = Minimizer(profile.objective, fit_params, fcn_args=(fitT, fit_data), calc_covar=True)
+    #minner = Minimizer(profile.objective, fit_params, fcn_args=(T, fit_data), calc_covar=True)
     result = minner.minimize()
     #report_fit(result)
 
@@ -124,7 +169,13 @@ def main(argv=None):
 
     # write density fit
     nfit = 500
-    fit_T = np.linspace(min(T),profile.T_c,num=nfit)
+    if parser.parse_args().type == 'ucst': 
+        Tlow = min(T)
+        Tlow = Tmin
+    elif parser.parse_args().type == 'lcst': 
+        Tlow = max(T)
+        Tlow = Tmax
+    fit_T = np.linspace(Tlow,profile.T_c,num=nfit)
 
     otp = open(parser.parse_args().fit_file, 'w') 
     otp.write('# T rho_v rho_l\n') 
